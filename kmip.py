d7fe99d5377f9bfea9d223e03500ca4699ca4644
Chris Smith <csmith@hashicorp.com>
2024-04-09 16:43:30 -0500

    Added some logic. Not muich work done

--- kmip.py
+++ kmip.py
@@ -7,14 +7,17 @@ class KMIP_Reporter:
 
     def build_kmip_report(self) -> dict:
         kmip_report = dict()
-        kmip_report["test"] = "done"
+        total_client_count = 0
 
-        # build namespace list
-        # namespaces = self.vault_client.get_namespaces()
-
-        # loop through
-        # build summary
-        # return the report
+        # build namespace list and get kmip namespaces
+        namespaces = self.vault_client.get_child_namespaces()
+        for namespace in namespaces.values():
+            kmip_mounts = self.vault_client.get_secret_mounts_by_type("kmip", namespace)
+            if len(kmip_mounts) > 0:
+                kmip_report[namespace] = kmip_mounts
+                # loop through
+                # build summary
+                # return the report
         return kmip_report
 
     def examine_namespace(self, namespace: str):
